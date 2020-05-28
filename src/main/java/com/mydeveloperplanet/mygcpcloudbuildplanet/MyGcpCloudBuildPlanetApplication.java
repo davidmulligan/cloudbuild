66bb7d1c798947b464f487e2a8185d1a4bc43216
David Mulligan <davemulligan@itg.co.uk>
2020-05-28 23:25:17 +0100

    first commit

---
+++ src/main/java/com/mydeveloperplanet/mygcpcloudbuildplanet/MyGcpCloudBuildPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.mygcpcloudbuildplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGcpCloudBuildPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGcpCloudBuildPlanetApplication.class, args);
+	}
+
+}
